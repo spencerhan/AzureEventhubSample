69ff9a495b4236b99f34daa599dd082deb8c541c
ouhshuo 麥普 <spencer.default.dev@gmail.com>
2022-08-03 14:57:00 +1200

    update python template

--- python/telemetry.py
+++ python/telemetry.py
@@ -3,38 +3,43 @@ import json
 from csv import reader
 from azure.eventhub import EventHubProducerClient, EventData
 
-connection_str = '<Your connection string>'
-eventhub_name = 'weather'
+# TO-DO adding data ouput a
+connection_str = '<CONNECTION STRING>'
+eventhub_name = '<EVENTHUB NAME>'
+
 
 class SampleData:
-    def __init__(self, date, city, description):
-        self.date = date
-        self.city = city
-        self.description = description
+    def __init__(self, attr1, attr2, attr3):
+        self.attr1 = attr1
+        self.attr2 = attr2
+        self.attr3 = attr3
 
     def __str__(self):
-        return f"{self.date}, {self.city}, {self.description}"
+        return f"{self.attr1}, {self.attr2}, {self.attr3}"
+
 
 def send_to_eventhub(client, data):
     event_data_batch = client.create_batch()
     event_data_batch.add(EventData(data))
     client.send_batch(event_data_batch)
 
-def main():
-    with open('weather_description.csv', 'r') as weather_descriptions:
-        csv_reader = reader(weather_descriptions)
-        header = next(weather_descriptions)
 
-        cities = header[:-1].split(',')[1:]
+def main():
+    with open('<DATA>', 'r') as _data:
+        csv_reader = reader(_data)
+        header = next(_data)
+
+        attr2s = header[:-1].split(',')[1:]
         client = EventHubProducerClient.from_connection_string(conn_str=connection_str, eventhub_name=eventhub_name)
         for row in csv_reader:
             date = row[0]
             i = 1
-            for city in cities:
-                sampleBatch = SampleData(date, city, row[i])
+            for attr2 in attr2s:
+                sampleBatch = SampleData(date, attr2, row[i])
                 send_to_eventhub(client, json.dumps(sampleBatch.__dict__))
                 print(json.dumps(sampleBatch.__dict__))
                 i += 1
             time.sleep(3)
+
 
 main()
